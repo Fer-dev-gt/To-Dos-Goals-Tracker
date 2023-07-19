977a3a0f42eb382d8782f4d32f856b2994d7588e
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-07-18 23:43:22 -0600

    First commit and preparing environment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './platzi.webp';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function App() {                                          // Esto es un Componente de React, por convención se escriben con mayuscula
+  return (                                                // Esto es lo que retorna nuestro Componente, son sus elementos internos, NO ES UN COMPONENTE, lo de abajo NO ES HTML, es JSX una sintaxis que facilita la lectura de código y luego se reenderiza a HTML clásico
+    <div className="App">     
+      <TodoItem />                                         
+      <TodoItem />
+      <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +18,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey! It's time to learn some React
         </a>
       </header>
     </div>
   );
 }
 
+
+// Creamos un nuevo Componente de React, este lo vamos a insertar en el Componente App
+function TodoItem() {
+  return(                                               // Creamos nuestra estructura JSX
+    <li>
+      <span>V</span>
+      <p>Cry with the 'Llorona'</p>
+      <span>X</span>
+    </li>
+  );
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';                                            // Importamos el Componente App del archivo 'App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Creamos un Root para React
+root.render(<App />);                                               // Reenderisamos el Componente App dentro del 'root' que creamos
