7c607450e16c482383dcc6928247c370d065d77c
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-07-20 12:15:13 -0600

    First implementation of State and change of State with setState

--- src/CreateToDoButton.js
+++ src/CreateToDoButton.js
@@ -2,7 +2,15 @@ import './CreateToDoButton.css';
 
 function CreateToDoButton() {
   return(                                               
-    <button className='CreateToDoButton'>+</button>
+    <button className='CreateToDoButton'
+      onClick={
+        (event) => {
+          console.log('le diste click')
+          console.log(event)
+          console.log(event.target)
+        }
+      }
+    >+</button>
   );
 }
 
--- src/ToDoSearch.js
+++ src/ToDoSearch.js
@@ -1,12 +1,21 @@
-import './ToDoSearch.css'
+import React from 'react';
+import './ToDoSearch.css';
 import searchIcon from "./Assets/search-icon.svg";
 
 function ToDoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');                           // Definimos un estado (dentro de un array), el estado no solo se consume tambien se actualiza el estado es inmutable, inicial vació con ''
+  
+  console.log(`Los usuarios buscan To Do's de ${searchValue}`);
+
   return(
     <div className='search-container'>
       <input 
       placeholder='Cut onions'
       className='ToDoSearch'
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
       />
       <img className='search-icon' src={searchIcon} alt='search icon'/>
     </div>
