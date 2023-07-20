ab185e38396ab66f25c63cac932b012b6cef67d6
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-07-19 23:39:48 -0600

    Implemented first css design

--- src/ToDoSearch.js
+++ src/ToDoSearch.js
@@ -1,11 +1,15 @@
 import './ToDoSearch.css'
+import searchIcon from "./Assets/search-icon.svg";
 
 function ToDoSearch() {
   return(
-    <input 
-    placeholder="Cut onions"
-    className='ToDoSearch'
-    />
+    <div className='search-container'>
+      <input 
+      placeholder='Cut onions'
+      className='ToDoSearch'
+      />
+      <img className='search-icon' src={searchIcon} alt='search icon'/>
+    </div>
   );
 }
 
