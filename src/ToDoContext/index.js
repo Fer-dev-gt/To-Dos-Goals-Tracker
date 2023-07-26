d5218c6a0f97542dbee38de7e412640f891c415e
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-07-25 21:23:54 -0600

    Finalized add new To Do to LocalStorage

--- src/ToDoContext/index.js
+++ src/ToDoContext/index.js
@@ -21,10 +21,20 @@ function ToDoProvider({ children }) {
     toDo.text.toLowerCase().includes(searchValue.toLowerCase())
   );
 
+  const addToDo = (text) => {
+    const newToDos = [...toDos];                                            
+    newToDos.push({
+      text,
+      completed: false,
+    });
+    saveToDos(newToDos);
+  };
+
   const completeToDo = (text) => {                                          // Función que cambia el State de 'toDos' en el apartado de 'completed' de false a true
     const newToDos = [...toDos];                                            // Creamos una copia del Array 'ToDos' usando destructuración [...]
     const toDoIndex = newToDos.findIndex( (toDo) =>                         // Encontramos el index del elemento del Array de ToDos que queremos cambiar
-      toDo.text === text );
+      toDo.text === text 
+    );
     newToDos[toDoIndex].completed = !newToDos[toDoIndex].completed;
     saveToDos(newToDos);
   };
@@ -32,7 +42,8 @@ function ToDoProvider({ children }) {
   const deleteToDo = (text) => {                                            // Función que crea un nuevo Array de los toDos sin el ToDos que le dimos X para eliminar
     const newToDos = [...toDos];                                            
     const toDoIndex = newToDos.findIndex( (toDo) =>
-      toDo.text === text);
+      toDo.text === text
+    );
     newToDos.splice(toDoIndex, 1);                                          // Quitamos el ToDo seleccionado con la X del Array de ToDos que luego va a actualizar el estado
     saveToDos(newToDos);
   };  
@@ -47,6 +58,7 @@ function ToDoProvider({ children }) {
       searchValue,
       setSearchValue,
       searchedToDos, 
+      addToDo,
       completeToDo, 
       deleteToDo,
       openModal,
--- src/ToDoForm/index.js
+++ src/ToDoForm/index.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import { ToDoContext } from '../ToDoContext';
 import './ToDoForm.css';
 
 function ToDoForm() {
-  return(
-    <form onSubmit={(event) => {
+  const {
+    addToDo,
+    setOpenModal,
+  } = React.useContext(ToDoContext);
+  
+  const[newToDoValue, setNewToDoValue] = React.useState('');                                // State local para que solo se use el valor final a enviar y no letra por letra
+
+  const onSubmit = (event) => {
       event.preventDefault();
-    }}>
+      addToDo(newToDoValue);
+      setOpenModal(false);
+  };
+
+  const onCancel = () => {
+      setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+      setNewToDoValue(event.target.value);
+  };
+
+  return(
+    <form onSubmit={onSubmit}>
       <label>Write your new To Do Here</label>
       <textarea
         placeholder='Cut onions for lunch'
+        value={newToDoValue}
+        onChange={onChange}
+        required
       />
       <div className='ToDoForm-buttonContainer'>
         <button 
           type=''
           className='ToDoForm-button ToDoForm-button--cancel'
-        >Cancel
-        </button>
+          onClick={onCancel}
+        >Cancel</button>
         <button 
           type='submit'
           className='ToDoForm-button ToDoForm-button--add'
-        >Add
-        </button>
+        >Add</button>
       </div>
     </form>
   )
